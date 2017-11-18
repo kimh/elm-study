06d71c1d45e5a577730271b611b60023ece1b850
Kim, Hirokuni <yangkookkim@gmail.com>
2017-11-18 20:56:45 +0900

    wip

--- Line.elm
+++ Line.elm
@@ -4,32 +4,48 @@ import Collage.Render exposing (svg)
 import Color exposing (..)
 import Html exposing (Html, button, div)
 import Html.Events exposing (onClick)
+import List exposing (..)
+import Debug exposing (..)
 
 main =
     Html.beginnerProgram { model = model, view = view, update = update }
 
 -- TODO: fix this compile error
-type alias Model = Float
+type alias Model = List Float
 type Msg = Matryoshka
 
 model : Model
-model = 800
+model = [800]
 
-
-update : Msg -> Model -> Float
+update : Msg -> Model -> List Float
 update msg model =
     case msg of
         Matryoshka ->
-            model / 2
+            let
+                lastElement = head model
+            in
+                case lastElement of
+                    Nothing ->
+                        [800]
+                    Just val ->
+                        val / 2 :: model
 
-renderTriangle : Model -> Html Msg
-renderTriangle base =
+makeTriangle : Float -> Collage Msg
+makeTriangle base =
      let
          gap = spacer 50 50
-         outerTri = triangle base
-                  |> filled (uniform Color.red)
      in
-         group [outerTri]
+         triangle base
+                  -- todo: generate random color
+                  |> filled (uniform (rgb 51 255 255))
+
+renderTriangle : Model -> Html Msg
+renderTriangle coll =
+     let
+         triangles = map makeTriangle coll
+     in
+         log (toString coll)
+         group triangles
          |> svg
 
 view : Model -> Html Msg
